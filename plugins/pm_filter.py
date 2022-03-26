2df73ef4b497ba27985868723ec59e4699ee3e6d
dk [OFLINE] <98028377+vloggerdeven@users.noreply.github.com>
2022-03-26 20:14:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -717,8 +717,8 @@ async def auto_filter(client, msg, spoll=False):
     else:
         await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
     if spoll:
-        await asyncio.sleep(60)
-        await msg.message.delete()
+        await asyncio.sleep(40)
+        await k.delete()
 
 
 async def advantage_spell_chok(msg):
