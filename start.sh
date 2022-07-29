0cfbeba583d63c64fed52f292b4e4f67ee3f680b
Abidpknew <105457327+Abidpknew@users.noreply.github.com>
2022-07-30 01:49:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tony-Stark2021/MH-BOT.git /MH-BOT
+  git clone https://github.com/Abidpknew/filteroli.git /filteroli
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MH-BOT
+  git clone $UPSTREAM_REPO /filteroli
 fi
-cd /MH-BOT
+cd /filteroli
 pip3 install -U -r requirements.txt
-echo "sᴛᴀʀᴛɪɴɢ 𝗗𝗞 𝗕𝗢𝗧𝘅........"
+echo "𝖲𝗍𝖺𝗋𝗍𝗂𝗇𝗀 𝖲𝗎𝗓𝗓𝗒 𝖻𝗒 #𝗔𝗯𝗢𝘂𝘁𝗠𝗲_𝗗𝗞..."
 python3 bot.py
